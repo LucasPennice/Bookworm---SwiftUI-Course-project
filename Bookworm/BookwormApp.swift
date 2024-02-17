b151fd3e06761473c128e0a7958bd53d763d72dd
Lucas Pennice <lucaspennice@gmail.com>
2024-02-17 09:24:39 -0300

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Lucas Pennice on 17/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
